24eddd42522567db773801ffabe3a81ac6a83c80
Felipe-Ortega <felipe.osouza@sptech.school>
2025-11-24 10:53:29 -0300

    criptografia de senha implementada

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from '../database/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly prismaService: PrismaService;
+
+  async createUser(data: Prisma.UserCreateInput) {
+    const hash = await bcrypt.hash(data.password, 10);
+    return this.prismaService.user.create({ 
+    data: {
+      ...data,
+      password: hash
+    }
+  });
+  }
+  async updateUser(params: {
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
+  }): Promise<User> {
+    const { where, data } = params;
+    return this.prismaService.user.update({
+      data,
+      where,
+    });
+  }
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prismaService.user.delete({
+      where,
+    });
+  }
+
+  async user(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: userWhereUniqueInput,
+    });
+    
+}}
